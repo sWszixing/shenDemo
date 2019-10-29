b8c8ba2159b4c91c2776db01c11c00fd5406ecc5
sWszixing <2786886635@qq.com>
2019-10-29 16:19:03 +0800

    demo2.py第二次修改！

--- demo2.py
+++ demo2.py
@@ -1,3 +1,6 @@
 for i in range(20):
-   print("第{}次输入！".format(i))
+     print("第{}次输入！".format(i))
 print("运行结束，第一次修改完毕！")
+for j in range(10):
+    print("第{}次输入！".format(j))
+print("运行结束,第二次修改完毕!")
